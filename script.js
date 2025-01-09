024eaa6a946eff5de9823b266e9c4387be3f6c42
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-08 22:40:35 -0500

    added count

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var count = 1;
+var heckYeahCount = 0;
 
-const coolButton = document.getElementById("coolbutton");
+const coolButton = document.getElementById("cool-button");
+const heckYeahs = document.getElementById("heck-yeahs");
 
 coolButton.addEventListener("click", coolFunction);
 
 function coolFunction() {
   count += 1;
   if (count % 2 === 0) {
+    heckYeahCount += 1;
     coolButton.style.backgroundImage = "url('./assets/button-gif.webp')";
+    heckYeahs.textContent = `heck yeahs: ${heckYeahCount}`;
   } else {
     coolButton.style.backgroundImage = null;
   }
