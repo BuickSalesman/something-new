78a165e9bba6ad1d0ad29c6dcdf337b8f17766eb
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-08 22:24:33 -0500

    turned button off and on

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+var count = 1;
+
 const coolButton = document.getElementById("coolbutton");
 
 coolButton.addEventListener("click", coolFunction);
 
 function coolFunction() {
-  console.log("Cool words");
+  count += 1;
+  if (count % 2 === 0) {
+    coolButton.style.backgroundImage = "url('./assets/button-gif.webp')";
+  } else {
+    coolButton.style.backgroundImage = null;
+  }
 }
