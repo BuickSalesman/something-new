5bcd8a8fd294f70d0c40b2e7913f524e2c236e1d
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-09 00:49:24 -0500

    heck yeah timer complete

--- script.js
+++ script.js
@@ -28,7 +28,45 @@ function coolFunction() {
 function startHeckYeahTimer() {
   interval = setInterval(() => {
     time += 1;
-    heckYeahTimer.textContent = `time heck yeahing: ${time}`;
+    if (time >= 0 && time < 10) {
+      heckYeahTimer.textContent = `time heck yeahing: 00:00:00:0${time}`;
+    } else if (time >= 10 && time < 100) {
+      heckYeahTimer.textContent = `time heck yeahing: 00:00:00:${time.toString().slice(-1)}${time
+        .toString()
+        .slice(0, 1)}`;
+    } else if (time >= 100 && time < 1000) {
+      heckYeahTimer.textContent = `time heck yeahing: 00:00:0${time.toString().slice(0, 1)}:${time
+        .toString()
+        .slice(1, 2)}${time.toString().slice(2, 3)}`;
+    } else if (time >= 1000 && time < 10000) {
+      heckYeahTimer.textContent = `time heck yeahing: 00:00:${time.toString().slice(0, 1)}${time
+        .toString()
+        .slice(1, 2)}:${time.toString().slice(2, 3)}${time.toString().slice(3, 4)}`;
+    } else if (time >= 10000 && time < 100000) {
+      heckYeahTimer.textContent = `time heck yeahing: 00:0${time.toString().slice(0, 1)}:${time
+        .toString()
+        .slice(1, 2)}${time.toString().slice(2, 3)}:${time.toString().slice(3, 4)}${time.toString().slice(4, 5)}`;
+    } else if (time >= 100000 && time < 1000000) {
+      heckYeahTimer.textContent = `time heck yeahing: 00:${time.toString().slice(0, 1)}${time
+        .toString()
+        .slice(1, 2)}:${time.toString().slice(2, 3)}${time.toString().slice(3, 4)}:${time.toString().slice(4, 5)}${time
+        .toString()
+        .slice(5, 6)}`;
+    } else if (time >= 1000000 && time < 10000000) {
+      heckYeahTimer.textContent = `time heck yeahing: 0${time.toString().slice(0, 1)}:${time
+        .toString()
+        .slice(1, 2)}${time.toString().slice(2, 3)}:${time.toString().slice(3, 4)}${time.toString().slice(4, 5)}:${time
+        .toString()
+        .slice(5, 6)}${time.toString().slice(6, 7)}`;
+    } else if (time >= 10000000 && time < 100000000) {
+      heckYeahTimer.textContent = `time heck yeahing: ${time.toString().slice(0, 1)}${time
+        .toString()
+        .slice(1, 2)}:${time.toString().slice(2, 3)}${time.toString().slice(3, 4)}:${time.toString().slice(4, 5)}${time
+        .toString()
+        .slice(5, 6)}:${time.toString().slice(6, 7)}${time.toString().slice(7, 8)}`;
+    } else {
+      heckYeahTimer.textContent = "time heck yeahing: heck yeah B)";
+    }
   }, 10);
 }
 
